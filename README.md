b0b497e8e3c5f7bf514b3ec436f4760d0f621837
Dinh Duong <0961020@gmail.com>
2019-04-09 10:58:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# The template for Chrome extension by Typescript, Webpack and (optional) Vue
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const Visualizer = require('webpack-visualizer-plugin');
+
+module.exports = {
+  entry: {
+    background: './src/background.ts',
+    content: './src/content.ts',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      // {
+      //   test: /\.vue$/,
+      //   loader: 'vue-loader'
+      // },
+      // {
+      //   test: /\.css$/,
+      //   use: ['vue-style-loader', 'css-loader']
+      // },
+      // {
+      //   test: /\.scss|svg$/,
+      //   use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      // },
+      {
+        test: /.png$/,
+        loader: 'url-loader'
+      }
+    ]
+  },
+  // resolve: {
+  //   extensions: ['.ts', '.js'],
+  //   alias: {
+  //     vue: 'vue/dist/vue.js',
+  //     // jquery: 'jquery/src/jquery'
+  //   }
+  // },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  plugins: [
+    // new VueLoaderPlugin(),
+    new Visualizer({
+      filename: './statistics.html'
+    })
+  ]
+};
