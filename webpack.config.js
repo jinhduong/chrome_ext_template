1f0c57b8320ed797919e56228254247ea3ec5edb
Dinh Duong <0961020@gmail.com>
2019-04-09 11:03:16 +0800

    Vue branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const Visualizer = require('webpack-visualizer-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
   entry: {
     background: './src/background.ts',
-    content: './src/content.ts',
+    content: './src/content.ts'
   },
   mode: 'development',
   module: {
@@ -14,38 +15,38 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/
       },
-      // {
-      //   test: /\.vue$/,
-      //   loader: 'vue-loader'
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use: ['vue-style-loader', 'css-loader']
-      // },
-      // {
-      //   test: /\.scss|svg$/,
-      //   use: ['vue-style-loader', 'css-loader', 'sass-loader']
-      // },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss|svg$/,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader']
+      },
       {
         test: /.png$/,
         loader: 'url-loader'
       }
     ]
   },
-  // resolve: {
-  //   extensions: ['.ts', '.js'],
-  //   alias: {
-  //     vue: 'vue/dist/vue.js',
-  //     // jquery: 'jquery/src/jquery'
-  //   }
-  // },
+  resolve: {
+    extensions: ['.ts', '.js'],
+    alias: {
+      vue: 'vue/dist/vue.js'
+      // jquery: 'jquery/src/jquery'
+    }
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
   plugins: [
-    // new VueLoaderPlugin(),
+    new VueLoaderPlugin(),
     new Visualizer({
       filename: './statistics.html'
     })
