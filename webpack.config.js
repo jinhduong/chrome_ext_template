8cbe849dfff207aae83b853af09a8b4607ac9bde
Dinh Duong <0961020@gmail.com>
2019-04-09 11:04:13 +0800

    Clean master

--- webpack.config.js
+++ webpack.config.js
@@ -14,38 +14,18 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/
       },
-      // {
-      //   test: /\.vue$/,
-      //   loader: 'vue-loader'
-      // },
-      // {
-      //   test: /\.css$/,
-      //   use: ['vue-style-loader', 'css-loader']
-      // },
-      // {
-      //   test: /\.scss|svg$/,
-      //   use: ['vue-style-loader', 'css-loader', 'sass-loader']
-      // },
       {
         test: /.png$/,
         loader: 'url-loader'
       }
     ]
   },
-  // resolve: {
-  //   extensions: ['.ts', '.js'],
-  //   alias: {
-  //     vue: 'vue/dist/vue.js',
-  //     // jquery: 'jquery/src/jquery'
-  //   }
-  // },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
   },
   devtool: 'source-map',
   plugins: [
-    // new VueLoaderPlugin(),
     new Visualizer({
       filename: './statistics.html'
     })
